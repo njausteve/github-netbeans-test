55acc47a669533ef2628383839c6ab11e309a5e2
njaus <njaus@WIN-2LN419BGKK3>
2015-05-18 15:38:54 -0700

    github test

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author njaus
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=10, y=40;
+        System.out.println("the product x & y is:"+x*y);
+        System.out.println("the sum x & y is:"+ x+y);
+        
+        
+ 
+        
+    }
+    
+}
